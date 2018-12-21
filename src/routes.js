0764183fa5312dbb7b7d6aad762fc951a23ff3c1
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2018-12-21 11:57:24 -0700

    turn in

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import {Switch, Route} from 'react-router-dom';
+import ClassList from './components/ClassList/ClassList';
+import Student from './components/Student/Student';
+
+export default (
+    <Switch>
+
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/classlist/:class' component={ClassList} />
+        <Route component={ Student } path='/student/:id' />
+        
+
+    </Switch>
+)
